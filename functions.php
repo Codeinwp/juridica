b483fcbd0ae36d687dab6b712801493e1fab4131
Dragos BUBU <i_am_bubudragos@yahoo.com>
2015-02-20 13:17:27 +0200

    Added script for div sorting
    #

--- functions.php
+++ functions.php
@@ -1,31 +1,23 @@
 <?php
-
 // add any new or customised functions here
 
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
 
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-
 	// Loads our main stylesheet.
 	wp_enqueue_style( 'juridica-child-style', get_stylesheet_uri(), array('style'), 'v1' );
 
 }
 
 
-/* 
-function zblackbeard_custom_script_fix()
+function juridica_custom_script_fix()
 {
-	if ( !wp_is_mobile() ){
+//  wp_enqueue_script('lawyeria_script_child', get_stylesheet_directory_uri() .'../juridica/js/wrapall.js', array(''), '201202067', true); 
 
-		wp_enqueue_script('zerif_script_child', get_stylesheet_directory_uri() .'/js/zerif.js', array('zerif_scrollReveal_script'), '201202067', true); 
-
-	}else{
-
-		wp_enqueue_script('zerif_script_child', get_stylesheet_directory_uri() .'/js/zerif.js', array(), '201202067', true); 
-
-	}
+	wp_register_script('lawyeria_script_child', get_stylesheet_directory_uri().'/js/wrapall.js', array('jquery') );
+    wp_enqueue_script('lawyeria_script_child');
 	
 }
 
-add_action( 'wp_enqueue_scripts', 'zblackbeard_custom_script_fix', 100 ); */
+add_action( 'wp_enqueue_scripts', 'juridica_custom_script_fix', 100 );
