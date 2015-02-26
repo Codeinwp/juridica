42f4fa696676bfa05f7097414c019eb0f4cc8a4d
Dragos BUBU <i_am_bubudragos@yahoo.com>
2015-02-26 16:34:02 +0200

    Feedback Changes
    Removed comments
    Fixed functions and script name
    Updated screenshot

--- functions.php
+++ functions.php
@@ -6,29 +6,22 @@ function theme_enqueue_styles() {
 
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 	// Loads our main stylesheet.
-	wp_enqueue_style( 'juridica-child-style', get_stylesheet_uri(), array('style'), 'v1' );
+	wp_enqueue_style( 'juridica-child-style', get_stylesheet_uri(), array('parent-style'), 'v1' );
 
 }
 
 
 function juridica_custom_script_fix()
 {
-//  wp_enqueue_script('lawyeria_script_child', get_stylesheet_directory_uri() .'../juridica/js/wrapall.js', array(''), '201202067', true); 
-
-	//wp_register_script('lawyeria_script_child', get_stylesheet_directory_uri().'/js/wrapall.js', array('jquery') );
     wp_enqueue_script('lawyeria_script_child');
-	
-	wp_enqueue_script('lawyeria_script_child', get_stylesheet_directory_uri().'/js/wrapall.js', array('jquery') );
-	wp_enqueue_script('juridica-nicescroll',get_stylesheet_directory_uri().'/js/jquery.nicescroll.js',array('jquery'),'12121',true);
+	wp_enqueue_script('lawyeria_script_child',     get_stylesheet_directory_uri().'/js/wrapall.js', array('jquery') );
+	wp_enqueue_script('juridica-nicescroll',   	   get_stylesheet_directory_uri().'/js/jquery.nicescroll.js',array('jquery'),'12121',true);
     wp_enqueue_script('juridica-nicescroll-script',get_stylesheet_directory_uri().'/js/juridica-nicescroll.js',array('jquery','juridica-nicescroll'),'12121',true);	
-	
 }
 
 add_action( 'wp_enqueue_scripts', 'juridica_custom_script_fix', 100 );
 
-
-
-function my_inline_styles()
+function juridica_inline_styles()
 		{
 		?>
 		<style type="text/css">
@@ -46,4 +39,4 @@ function my_inline_styles()
 		<?php
 		}
 
-add_action("wp_head","my_inline_styles");
+add_action("wp_head","juridica_inline_styles");
