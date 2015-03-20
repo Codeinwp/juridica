81edd856e8133e30fc487dbe7b864777eaee6b17
Ionut Neagu <ionut.neagu@vertistudio.com>
2015-03-20 13:51:35 +0100

    Changed how we add parent style

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
 add_action( 'wp_enqueue_scripts', 'juridica_enqueue_styles' );
 function juridica_enqueue_styles() {
 
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    	wp_register_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 	// Loads our main stylesheet.
 	wp_enqueue_style( 'juridica-child-style', get_stylesheet_uri(), array('parent-style'), 'v1' );
 
@@ -15,7 +15,7 @@ function juridica_custom_script_fix()
    
 	wp_enqueue_script('juridica_script_child',get_stylesheet_directory_uri().'/js/wrapall.js', array('jquery') );
 	wp_enqueue_script('juridica-nicescroll',get_stylesheet_directory_uri().'/js/jquery.nicescroll.js',array('jquery'),'12121',true);
-    wp_enqueue_script('juridica-nicescroll-script',get_stylesheet_directory_uri().'/js/juridica-nicescroll.js',array('jquery','juridica-nicescroll'),'12121',true);	
+        wp_enqueue_script('juridica-nicescroll-script',get_stylesheet_directory_uri().'/js/juridica-nicescroll.js',array('jquery','juridica-nicescroll'),'12121',true);	
 }
 
 add_action( 'wp_enqueue_scripts', 'juridica_custom_script_fix', 100 );
